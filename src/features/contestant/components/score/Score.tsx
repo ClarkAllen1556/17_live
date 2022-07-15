7617ba6e9470232f1f3c4030b6da45f73a284083
AC1556 <allenclark@u.boisestate.edu>
2022-07-15 15:44:21 +0900

    Animating score increase

--- src/features/contestant/components/score/Score.tsx
+++ src/features/contestant/components/score/Score.tsx
@@ -1,9 +1,18 @@
 import { StyledScore } from '~/features/contestant/components/score/score.styled';
+import CountUp from 'react-countup';
+
+import { usePreviousValue } from '~/common/hooks/previousValue.hooks';
 
 interface IProps {
   score: number;
 }
 
 export default function Score({ score }: IProps) {
-  return <StyledScore>{score}</StyledScore>;
+  const previousValue = usePreviousValue(score);
+
+  return (
+    <StyledScore>
+      <CountUp start={previousValue} end={score} duration={5} />
+    </StyledScore>
+  );
 }
