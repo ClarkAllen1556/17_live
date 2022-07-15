c3b950fb5f93ffcdac12077e2170979808f8acb1
AC1556 <allenclark@u.boisestate.edu>
2022-07-15 11:16:06 +0900

    using `useInterval` hook to update scores

--- src/features/feed/Feed.tsx
+++ src/features/feed/Feed.tsx
@@ -1,25 +1,45 @@
 import { useEffect } from 'react';
-
 import { useAppDispatch, useAppSelector } from '~/common/hooks/store.hooks';
-
 import Contestant from '~/features/contestant/Contestant';
 import { populateFeed, setFeed } from '~/features/feed/feed.slice';
+import randomInteger from '~/utils/randomInteger';
+import { IContestant } from '~/common/types/Contestant.interface';
+
+import { useInterval } from '~/common/hooks/interval.hooks';
 
 export default function Feed() {
+  const isLoading = useAppSelector((state) => state.loadingStatus.isLoading);
   const contestants = useAppSelector((state) => state.feed.contestantMap);
   const dispatch = useAppDispatch();
 
+  useInterval(() => {
+    const userIds = Object.keys(contestants);
+    const index = randomInteger(userIds.length);
+
+    increaseScore(contestants[userIds[index]], randomInteger(100));
+  }, 500);
+
   useEffect(() => {
+    console.log('in use effect');
     dispatch(populateFeed());
   }, []);
 
-  function increaseScore(index: number) {
-    // dispatch(setFeed());
+  function increaseScore(contestant: IContestant, score: number) {
+    dispatch(
+      setFeed({
+        ...contestants,
+        [contestant.userID]: { ...contestant, score: contestant.score + score },
+      })
+    );
+  }
+
+  if (isLoading) {
+    return <div>Loading...</div>;
   }
 
   return (
     <>
-      {Object.entries(contestants).map(([key, value], i) => (
+      {Object.entries(contestants).map(([_, value], i) => (
         <Contestant contestant={value} key={i} />
       ))}
     </>
