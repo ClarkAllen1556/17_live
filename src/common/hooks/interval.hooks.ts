1f9e7990636ad98b68ee37d40eb29fd31ddc3e10
AC1556 <allenclark@u.boisestate.edu>
2022-07-15 15:45:36 +0900

    removing uneeded import

--- src/common/hooks/interval.hooks.ts
+++ src/common/hooks/interval.hooks.ts
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef } from 'react';
+import { useEffect, useRef } from 'react';
 
 // hook found in this blog post: https://overreacted.io/making-setinterval-declarative-with-react-hooks/
 // SO reference: https://stackoverflow.com/questions/71184843/how-to-update-state-using-setinterval-on-functional-components-in-react
