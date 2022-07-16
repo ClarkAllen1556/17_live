d356dad1259930f55ff811d3611f6ddc269cfea3
AC1556 <allenclark@u.boisestate.edu>
2022-07-16 12:46:26 +0900

    Abstracting Feed styles into own component file

--- src/features/feed/Feed.tsx
+++ src/features/feed/Feed.tsx
@@ -4,9 +4,8 @@ import Contestant from '~/features/contestant/Contestant';
 import { populateFeed, setFeed } from '~/features/feed/feed.slice';
 import randomInteger from '~/utils/randomInteger';
 import { IContestant } from '~/common/types/Contestant.interface';
-
 import { useInterval } from '~/common/hooks/interval.hooks';
-import styled, { keyframes } from 'styled-components';
+import { StyledOrderedList, StyledListItem } from '~/features/feed/feed.styled';
 
 export default function Feed() {
   const isLoading = useAppSelector((state) => state.loadingStatus.isLoading);
@@ -39,41 +38,15 @@ export default function Feed() {
 
   return (
     <StyledOrderedList>
-      {Object.entries(contestants).map(([_, value], i) => (
+      {Object.entries(contestants).map(([_, contestant], i) => (
         <StyledListItem
-          currentPosition={value.currentPosition}
-          previousPosition={value.previousPosition}
+          currentPosition={contestant.currentPosition}
+          previousPosition={contestant.previousPosition}
+          key={`${contestant.userID}-${i}`}
         >
-          <Contestant contestant={value} index={i} key={i} />
+          <Contestant contestant={contestant} index={i} />
         </StyledListItem>
       ))}
     </StyledOrderedList>
   );
 }
-
-const StyledOrderedList = styled.ol`
-  list-style-type: none;
-`;
-
-const StyledListItem = styled.li<{
-  previousPosition: number;
-  currentPosition: number;
-}>`
-  position: absolute;
-  height: 48px;
-  width: 320px;
-  top: ${({ currentPosition }) => currentPosition * 48}px;
-  animation: ${({ previousPosition, currentPosition }) =>
-      _moveContestant(previousPosition, currentPosition)}
-    0.5s ease;
-`;
-
-const _moveContestant = (prev: number, cur: number) => keyframes`
-  from {
-    top ${prev * 48}px;
-  }
-  
-  to {
-    top ${cur * 48}px;
-  }
-`;
---
+++ src/features/feed/feed.styled.ts
@@ -0,0 +1,28 @@
+import styled, { keyframes } from 'styled-components';
+
+export const StyledOrderedList = styled.ol`
+  list-style-type: none;
+`;
+
+export const StyledListItem = styled.li<{
+  previousPosition: number;
+  currentPosition: number;
+}>`
+  position: absolute;
+  height: 48px;
+  width: 320px;
+  top: ${({ currentPosition }) => currentPosition * 48}px;
+  animation: ${({ previousPosition, currentPosition }) =>
+      _moveContestant(previousPosition, currentPosition)}
+    0.5s ease;
+`;
+
+const _moveContestant = (prev: number, cur: number) => keyframes`
+  from {
+    top ${prev * 48}px;
+  }
+  
+  to {
+    top ${cur * 48}px;
+  }
+`;
